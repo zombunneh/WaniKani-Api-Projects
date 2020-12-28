5dbd9d686dade64d1b4505d3275a404fc4238cd2
zombunneh <matthew.winterbourn@hotmail.co.uk>
2020-12-28 00:23:14 +0000

    Added new enums to define certain response types, currently unused

---
+++ APIContact/src/com/api/json/ResponseDataType.java
@@ -0,0 +1,28 @@
+/*
+ * ResponseType.java
+ *
+ * @author Matthew Winterbourn
+ * @version 1.0
+ * @since 10/11/2020
+ *
+ * Copyright (c) 2020.
+ */
+
+package com.api.json;
+
+// Not currently in use
+public enum ResponseDataType {
+
+    ASSIGNMENTS,
+    KANJI,
+    LEVEL_PROGRESSION,
+    RADICAL,
+    RESET,
+    REVIEW_STATISTIC,
+    REVIEW,
+    SPACED_REPETITION_SYSTEM,
+    STUDY_MATERIAL,
+    USER,
+    VOCABULARY,
+    VOICE_ACTOR
+}
---
+++ APIContact/src/com/api/json/ResponseSubjectType.java
@@ -0,0 +1,18 @@
+/*
+ * ResponseSubjectType.java
+ *
+ * @author Matthew Winterbourn
+ * @version 1.0
+ * @since 10/11/2020
+ *
+ * Copyright (c) 2020.
+ */
+
+package com.api.json;
+
+//Not currently in use
+public enum ResponseSubjectType {
+    KANJI,
+    RADICAL,
+    VOCABULARY
+}
